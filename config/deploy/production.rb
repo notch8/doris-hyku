044d120ddc74ec6921474507aea9fe83b8bba581
Rob Kaufman <rob@notch8.com>
2018-12-11 11:02:37 -0800

    more deploy work

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '3.80.142.174', user: 'centos', roles: [:web, :app, :db, :resque_pool]
+server 'hyku.getinfo.nyc', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
