997b94ec710f218ebb489301f0ddbe2d43d257a3
Rob Kaufman <rob@notch8.com>
2019-01-07 23:57:24 -0800

    docker fixes for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.web_console.whitelisted_ips = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
