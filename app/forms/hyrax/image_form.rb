c00be230772dd9d68209d735335f63880f5921dd
Rob Kaufman <rob@notch8.com>
2019-01-30 10:15:30 -0800

    make extent show up

--- app/forms/hyrax/image_form.rb
+++ app/forms/hyrax/image_form.rb
@@ -4,7 +4,7 @@ module Hyrax
   class ImageForm < Hyrax::Forms::WorkForm
     include Hyrax::FormTerms
     self.model_class = ::Image
-    self.terms += %i[resource_type extent rendering_ids]
+    self.terms += %i[resource_type rendering_ids]
 
     def secondary_terms
       super - [:rendering_ids]
