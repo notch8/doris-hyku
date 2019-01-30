3e617fe12c679940dc19ff0f1e1e3733280288d8
Rob Kaufman <rob@notch8.com>
2019-01-30 10:03:53 -0800

    add extent to general work

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -1,9 +1,13 @@
 class GenericWork < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::CommonMetadata
-  include ::Hyrax::BasicMetadata
   include HasRendering
   validates :title, presence: { message: 'Your work must have a title.' }
+  property :extent, predicate: ::RDF::Vocab::DC.extent, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  include ::Hyrax::BasicMetadata
 
   # This indexer uses IIIF thumbnails:
   self.indexer = WorkIndexer
