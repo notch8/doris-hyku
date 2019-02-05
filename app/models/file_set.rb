929261e37c4a44e10c2bb41966a03f7dcc3e4f46
Rob Kaufman <rob@notch8.com>
2019-02-04 16:58:17 -0800

    clear file set cache after file sets

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -4,6 +4,7 @@ class FileSet < ActiveFedora::Base
   include ::Hyrax::FileSetBehavior
 
   before_destroy :remove_rendering_relationship
+  after_update :clear_cache
 
   # Hyku has its own FileSetIndexer: app/indexers/file_set_indexer.rb
   # It overrides Hyrax to inject IIIF behavior.
@@ -11,6 +12,11 @@ class FileSet < ActiveFedora::Base
 
   def rendering_ids
     to_param
+  end
+
+  # clean out RIIF cache when the file changes
+  def clear_cache
+   FileUtils.rm_f(Rails.root.join('tmp', 'network_files')) 
   end
 
   private
