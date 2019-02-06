c68a4f04a29a0bd63cde706eb3a3106fdafe44c1
Rob Kaufman <rob@notch8.com>
2019-02-06 10:47:20 -0800

    set tmp for prod

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
+ENV['TMPDIR'] = "/opt/doris-hyku/tmp" if ENV['RAILS_ENV'] == 'production'
 require 'bundler/setup' # Set up gems listed in the Gemfile.
