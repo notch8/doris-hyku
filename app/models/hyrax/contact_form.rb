dd725c9fc94ff5c775f01b6b25099b03b0679563
Rob Kaufman <rob@notch8.com>
2019-04-10 16:40:55 -0700

    seperate email form from and to

--- app/models/hyrax/contact_form.rb
+++ app/models/hyrax/contact_form.rb
@@ -18,7 +18,7 @@ module Hyrax
       # hyrax send the mail 'from' the submitter, which doesnt work on most smtp transports
       {
         subject: "#{Hyrax.config.subject_prefix} #{email} #{subject}",
-        to: Hyrax.config.contact_email,
+        to: 'photounit@records.nyc.gov',
         from: Settings.contact_email
       }
     end
