59a71c4c8368bb693735e6fed441cc7494894321
Rob Kaufman <rob@notch8.com>
2019-04-10 15:47:53 -0700

    path fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 every 1.day do
-  command "cd /opt/doris-hyku/current/ops/Backup && dotenv -f ../../.env 'backup perform --trigger hyku -c ./config.rb'"
+  command "cd /opt/doris-hyku/current/ops/Backups && dotenv -f ../../.env 'backup perform --trigger hyku -c ./config.rb'"
 end
 
 every 1.hour do
