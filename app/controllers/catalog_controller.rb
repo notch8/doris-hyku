67d5b9789f62315f55e521ad53ff1f25d7564be5
Rob Kaufman <rob@notch8.com>
2019-05-01 22:17:08 -0700

    fix collection show page search isseus

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -70,7 +70,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field solr_name("identifier", :stored_searchable), itemprop: 'identifier', link_to_search: solr_name("identifier", :facetable)
+    config.add_index_field solr_name("identifier", :stored_searchable), itemprop: 'identifier' #, link_to_search: solr_name("identifier", :facetable)
     config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false
     # config.add_index_field solr_name("description", :stored_searchable), itemprop: 'description', helper_method: :iconify_auto_link
     # config.add_index_field solr_name("keyword", :stored_searchable), itemprop: 'keywords', link_to_search: solr_name("keyword", :facetable)
